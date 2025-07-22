3c3aa974eea1be1862f9d0023615ab87a409b6c0
Hamilton613 <leonelcalderon939@gmail.com>
2025-07-22 11:54:59 -0600

    se agregaron letras

--- js/api4.js
+++ js/api4.js
@@ -6,7 +6,6 @@ async function fetchCharacters() {
         const response = await fetch(URL);
         const data = await response.json();
         const ubicacion = document.getElementById("ubicacion");
-
         data.forEach(character => {
             // Si no hay imagen o está vacía, usa la imagen por defecto
             const imgSrc = character.image && character.image.trim() !== "" ? character.image : defaultImage;
@@ -14,7 +13,7 @@ async function fetchCharacters() {
             const div = document.createElement("div");
             div.className = "card mb-3";
             div.style.width = "18rem";
-            div.style.margin = "10px";
+            div.style.margin = "10px";  
             div.innerHTML = `
                 <img src="${imgSrc}" class="card-img-top" alt="${character.name}" style="height: 250px; object-fit: contain;">
                 <div class="card-body">
